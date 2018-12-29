2a6f6fd8344242f67a89c4adfef2ba71e79185f1
Matteo Fasiolo <matteo.fasiolo@gmail.com>
2018-12-29 16:35:54 +0000

    Updated plotting code

--- Code_for_plots.R
+++ Code_for_plots.R
@@ -25,7 +25,11 @@ figure_1 <- list()
 figure_1[[1]] <- plot(sm(fit_gaus, 1)) + 
                  l_dens(type = "cond", n = c(50, 50), tol = -1, trans = function(.x) .x^(0.33)) +
                  l_fitLine() +
-                 labs(x = "t", y = expression(f[1] * "(t) (in 100s)")) + #ylim(-7500, 7500) + 
+                 labs(x = "t", y = expression(f[1] * "(t) (in 100s)")) + #ylim(-7500, 7500) +
+                 scale_fill_gradientn(colours = viridis(50, begin = 0.2), 
+                                      na.value = "white", 
+                                      name="p(r|t)") + 
+                 theme(legend.position="bottom") +
                  coord_cartesian(expand=F)
 
 figure_1[[4]] <- qq(fit_gaus, rep = 50, showReps = T, level = 0.99, method = "tnormal", CI = "normal", 
@@ -34,26 +38,35 @@ figure_1[[4]] <- qq(fit_gaus, rep = 50, showReps = T, level = 0.99, method = "tn
 
 figure_1[[2]] <- check1D(fit_gaus, x = "wM_s95", maxpo = 1e3, type = "tnormal") + 
                  l_densCheck(tol = -1) + 
-                 guides(fill=FALSE) + 
+                 xlim(0, 25) +
+                 scale_fill_gradient2(low = "red", mid = "white", high = "blue", 
+                                      midpoint = 0, limits = c(-0.7, 0.77)) +
+                 guides(fill=FALSE) +
                  labs('x' = expression(T^s), 'y' = "Quantile residuals") + 
-                 l_rug(mapping = aes(x = x, y = y)) # + coord_cartesian(expand=F)
+                 l_rug(mapping = aes(x = x)) + coord_cartesian(expand=F)
 
 figure_1[[3]] <- check1D(fit_gaus, x = "wM_s95", maxpo = 1e3, type = "scaled.pearson") + 
                  l_gridCheck1D(gridFun = function(.x) sd(.x), n = 30, stand = "none", showReps = F) +
-                 theme(panel.grid.minor = element_line(size = 0.1, colour = "grey"), 
-                 panel.grid.major = element_line(size = 0.1, colour = "grey")) + 
+                 theme_bw() +
+                 # theme(panel.grid.minor = element_line(size = 0.1, colour = "grey"), 
+                 # panel.grid.major = element_line(size = 0.1, colour = "grey")) + 
                  guides(fill=FALSE) + 
                  labs('x' = expression(T^s), "y" = "sd(r)")
 
 figure_1[[5]] <- check1D(fit_gaus, x = "Posan", maxpo = 1e3, type = "tnormal") + 
                  l_densCheck(tol = -1) + 
-                 guides(fill=FALSE) + labs('x' = "toy", 'y' = "Quantile residuals") + 
-                 l_rug(mapping = aes(x = x, y = y)) # + coord_cartesian(expand=F)
+  scale_fill_gradient2(low = "red", mid = "white", high = "blue", 
+                       midpoint = 0, limits = c(-0.7, 0.77), name = expression(delta (p, p[m]) )) +
+                 #guides(fill=FALSE) + 
+                 theme(legend.position="bottom") +
+                 labs('x' = "toy", 'y' = "Quantile residuals") + 
+                 l_rug(mapping = aes(x = x)) + coord_cartesian(expand=F)
 
 figure_1[[6]] <-  check1D(fit_gaus, x = "Posan", maxpo = 1e3, type = "scaled.pearson") + 
                   l_gridCheck1D(gridFun = function(.x) sd(.x), n = 30, stand = "none", showReps = F) +
-                  theme(panel.grid.minor = element_line(size = 0.1, colour = "grey"), 
-                        panel.grid.major = element_line(size = 0.1, colour = "grey")) + 
+                  theme_bw() +
+                  # theme(panel.grid.minor = element_line(size = 0.1, colour = "grey"), 
+                  #       panel.grid.major = element_line(size = 0.1, colour = "grey")) + 
                   guides(fill=FALSE) + 
                   labs('x' = "toy", "y" = "sd(r)")
 
@@ -70,7 +83,7 @@ labels_x <- round(nrow(data_UK_load) * c(0.15, 0.5, 0.85))
 responses_y <- c(figure_1[[1]]$layers[[1]]$data$y)
 ticks_y <- seq(min(responses_y), max(responses_y), length.out = 4)
 labels_y <- round(ticks_y / 100)
-figure_1[[1]] <- figure_1[[1]] + theme(legend.position="none") + 
+figure_1[[1]] <- figure_1[[1]] + # theme(legend.position="none") + 
   scale_x_continuous(breaks = ticks_x, labels = labels_x) + 
   scale_y_continuous(breaks = ticks_y, labels = labels_y)
 
@@ -92,8 +105,24 @@ for(xx in c("a", "c", "e", "b", "d", "f")){
   kk <- kk+1
 }
 
-# Plotting
-grid.arrange(grobs=figure_1, ncol = 3)
+fig_layout <- matrix(c(1, 1, 2, 2, 3, 3, 
+                       1, 1, 2, 2, 3, 3, 
+                       1, 1, 2, 2, 3, 3,
+                       1, 1, 2, 2, 3, 3,
+                       1, 1, 2, 2, 3, 3,
+                       1, 1, 2, 2, 3, 3,
+                       1, 1, 2, 2, 3, 3,
+                       1, 1, 5, 5, 3, 3,
+                       1, 1, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6,
+                       4, 4, 5, 5, 6, 6), 16, 6, byrow = T)
+
+grid.arrange(grobs=figure_1, layout_matrix = fig_layout)
 
 # Free some memory
 rm(fit_gaus); rm(fit)
@@ -128,7 +157,7 @@ figure_2[[1]] <- plot(sm(fit_gaus_loc_scale, 5), n = 200) +
 
 figure_2[[2]] <- plot(sm(fit_gaus_loc_scale, 6), n = 200) + l_ciPoly() + l_fitLine() + 
                  l_ciLine(colour = 2, linetype = 2) +  
-                 labs(x = "Instant", y = expression(v[2] * "(" * I * ")"))
+                 labs(x = "I", y = expression(v[2] * "(" * I * ")"))
 
 figure_2[[3]] <- plot(sm(fit_gaus_loc_scale, 7), n = 200) + l_ciPoly() + l_fitLine() + 
                  l_ciLine(colour = 2, linetype = 2) + ylim(-0.3, 1.21) +
@@ -147,12 +176,12 @@ set.seed(784)
 figure_2[[4]] <- check2D(fit_gaus_loc_scale, x1 = "wM_s95", x2 = "Instant") + 
                  l_gridCheck2D(bw = c(1.5, 3), gridFun = sd) + 
                  scale_fill_gradientn(colours = viridis(50,begin=0), na.value="white") + 
-                 coord_cartesian(expand=F) + labs(x = expression(T^s))
+                 coord_cartesian(expand=F) + labs(x = expression(T^s), y = "I")
 
 figure_2[[5]] <- check2D(fit_gaus_loc_scale, x1 = "Posan", x2 = "Instant") + 
                  l_gridCheck2D(bw = c(0.02, 1), gridFun = skewness) + 
                  scale_fill_gradientn(colours = viridis(50,begin=0), na.value="white") + 
-                 coord_cartesian(expand=F) + labs(x = expression("toy"))
+                 coord_cartesian(expand=F) + labs(x = expression("toy"), y = "I")
 
 figure_2[[6]] <- check1D(fit_gaus_loc_scale, x = "wM_s95", type = "deviance") + 
                  l_gridCheck1D(n = 30, gridFun = sd, stand = "none", showReps = F) +
@@ -162,7 +191,7 @@ figure_2[[6]] <- check1D(fit_gaus_loc_scale, x = "wM_s95", type = "deviance") +
 figure_2[[7]] <- check1D(fit_gaus_loc_scale, x = "Instant", type = "deviance") + 
                  l_gridCheck1D(n = 30, gridFun = skewness, stand = "none", showReps = F) +
                  theme_bw() +  guides(fill=FALSE) + 
-                 labs('x' = "Instant", "y" = "skew(r)")
+                 labs('x' = "I", "y" = "skew(r)")
 
 # Annotating each plot with a), b), ...
 kk <- 1
@@ -224,7 +253,7 @@ fit_profile <- getViz(fit_profile, nsim = 50)
 figure_3_and_4 <- list()
 figure_3_and_4[[1]] <- plot(sm(fit_shash, 8), n = 200) + l_ciPoly() + 
                        l_ciLine(colour = 2) + l_fitLine() +
-                       labs(x = "Instant", y = expression(s[1] * "(Instant)"))
+                       labs(x = "I", y = expression(s[1] * "(I)"))
 
 figure_3_and_4[[2]] <- qq(fit_shash, rep = 50, showReps = T, level = 0.99, 
                           method = "tnormal", type = "tnormal", worm = F, CI = "normal",
@@ -264,7 +293,7 @@ figure_3_and_4[[3]] <- check2D(fit_shash, x1 = "wM_s95", x2 = "Instant", type = 
                                   n = c(8, 8), mapping = aes(x=gx, y=gy, group = gid, colour = I(color)), 
                        height=8.5, width = 4, size = 0.1) + 
                        scale_fill_gradientn(colours = viridis(50, begin=0.4)) + 
-                        coord_cartesian(expand=F) + labs(x = expression(T^s)) 
+                        coord_cartesian(expand=F) + labs(x = expression(T^s), y = "I") 
 
 # Function that produces the data for the kernel density estimates glyphs in Figure 4
 # See ?l_glyphs2D to learn what data.frame ".d" contains and type of data.frame
@@ -282,7 +311,7 @@ figure_3_and_4[[4]] <- check2D(fit_shash, x1 = "Posan", x2 = "Instant", type = "
                                   n = c(10, 10), mapping = aes(x=gx, y=gy, group = gid), 
                                   height=4.5, width = 0.2) + 
                        scale_fill_gradientn(colours = viridis(50, begin=0.4)) + 
-                       coord_cartesian(expand=F) + labs(x = "toy") 
+                       coord_cartesian(expand=F) + labs(x = "toy", y = "I") 
 
 # Plot daily consumption profiles for Figure 3
 figure_3_and_4[[5]] <- plot(sm(fit_profile, 1), trans = function(.x) .x + coef(fit_profile)[1]) + 
@@ -361,19 +390,19 @@ figure_5[[1]] <- plot(sm(fit_shash, 2), n = 100, too.far = 0) +
   l_fitRaster(noiseup = T, pTrans = zto1(0.05, 2, 0.1)) + 
   l_fitContour() + 
   theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) + 
-  labs(x = "T", y = "Instant", "title" = NULL) + coord_cartesian(expand=F) 
+  labs(x = "T", y = "I", "title" = NULL) + coord_cartesian(expand=F) 
 
 figure_5[[2]] <- plot(sm(fit_shash, 3), n = 100, too.far = 0) + 
   l_fitRaster(noiseup = T, pTrans = zto1(0.05, 2, 0.1)) + 
   l_fitContour() + 
   theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) + 
-  labs(x = expression(T^s), y = "Instant", "title" = NULL) + coord_cartesian(expand=F) 
+  labs(x = expression(T^s), y = "I", "title" = NULL) + coord_cartesian(expand=F) 
 
 figure_5[[3]] <-  plot(sm(fit_shash, 4), n = 100, too.far = 0) + 
   l_fitRaster(noiseup = T, pTrans = zto1(0.05, 2, 0.1)) + 
   l_fitContour() + 
   theme(legend.position = "bottom", legend.key.width = unit(1, "cm")) + 
-  labs(x = "toy", y = "Instant", "title" = NULL) + coord_cartesian(expand=F) 
+  labs(x = "toy", y = "I", "title" = NULL) + coord_cartesian(expand=F) 
 
 # Rendering Figure 5
 grid.arrange(grobs = lapply(figure_5, "[[", "ggObj"), ncol = 3)
